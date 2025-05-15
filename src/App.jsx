00019f37a4024e0004cde8a15ea15f2a7d26d1f7
shahzebsoomro <shahzaibsoomro@gmai.com>
2025-05-15 18:05:48 +0500

    Learning react essentials

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function Header({name}) {
+  return <h2>{name} Header</h2>
+}
+function Footer({year}) {
+  return (
+  <>
+  <h2>Footer</h2>
+    <h3>Copyright {year}</h3>
+    </>
+  
+  )
+}
+
+function App() {
+  let name  = "Shahzeb Soomro"
+  return (
+    <>
+    <Header name="Mr." />
+      <h1>Hello {name.toUpperCase()}</h1>
+    <Footer year = '2025'/>
+    </>
+  )
+}
+
+export default App
